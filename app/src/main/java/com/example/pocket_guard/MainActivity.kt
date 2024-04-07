b993b673621f3b7d88c7a8cc02d0b0555aa3810c
yashbim <ybimsara03@gmail.com>
2024-04-08 01:23:33 +0530

    converted to linear layout

--- app/src/main/java/com/example/pocket_guard/MainActivity.kt
+++ app/src/main/java/com/example/pocket_guard/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.pocket_guard
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -26,20 +24,9 @@ class MainActivity : AppCompatActivity() {
 
             val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
             var interval : Int = 30000
-            var get_interval : EditText = findViewById(R.id.interval)
-
-            val intervalText = get_interval.text.toString()
             var intervalInt: Int
-            try {
-                intervalInt = intervalText.toInt()
-            } catch (e: NumberFormatException) {
-                // Handle the case where the text is not a valid integer
-                // For example, you might display an error message to the user
-                Log.e("Conversion Error", "Invalid integer value in EditText")
-                intervalInt = 0 // Or set a default value as needed
-            }
                 Toast.makeText(applicationContext,"Time interval set to 30s", Toast.LENGTH_SHORT).show()
-                vibrate(intervalInt,time)
+//                vibrate(intervalInt,time)
 
 
         }
